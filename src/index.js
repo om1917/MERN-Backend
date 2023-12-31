732061d5b6649ec0b25465bbdb0351c6ab0e3579
Om <om.bhaisora@gmail.com>
2023-12-31 13:24:56 +0530

    Utility code

--- src/index.js
+++ src/index.js
@@ -2,12 +2,24 @@
 
 import dotenv from "dotenv";
 import connectDB from "./database/index.js";
-
+import app from "./app.js";
 dotenv.config({
     path:'./env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.on("error", (error) => {
+        console.log("Error:", error);
+        throw error;
+    });
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("Connection Failed !",err)
+})
 
 /*
 import express from "express";
