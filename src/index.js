fbdecfb6dd5b50c38d75609d4aa71e12187f5365
Om <om.bhaisora@gmail.com>
2023-12-31 11:25:29 +0530

    mongo db connection

---
+++ src/index.js
@@ -0,0 +1,34 @@
+//require('dotenv').config({path:'./env'})
+
+import dotenv from "dotenv";
+import connectDB from "./database/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB();
+
+/*
+import express from "express";
+
+const app = express();
+
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("Error:", error);
+            throw error;
+        });
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Process is listening or port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.log("Error:", error)
+        throw error
+    }
+})()
+*/
