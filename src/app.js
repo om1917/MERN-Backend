c42cf84736f1bffdeb0f192fcaf394db17fbb557
Om <om.bhaisora@gmail.com>
2023-12-31 19:05:33 +0530

    All setup done

--- src/app.js
+++ src/app.js
@@ -20,4 +20,10 @@ app.use(express.static("public"));
 
 app.use(cookieParser());
 
+//Routes
+import userRouter from './routes/user.routes.js'
+
+//Routes Declaration
+app.use("/api/v1/users",userRouter)
+
 export default app;
